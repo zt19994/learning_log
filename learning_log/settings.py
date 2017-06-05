557550ed58c270f32e8c7d05d5ce56262d93b3e7
zt19994 <zt191610942@163.com>
2017-06-05 08:54:59 +0800

    setting

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -132,7 +132,7 @@ LOGIN_URL = '/users/login/'
 #django-bootstrap3的设置
 BOOTSTRAP3 = {
     'include_jquery': True,
-}
+    }
 
 #Heroku设置
 if os.getcwd() == '/app':
@@ -145,7 +145,9 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     #支持所有的主机头（host header）
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['learning-log.herokuapp.com']
+
+    DEBUG = False
 
     #静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
